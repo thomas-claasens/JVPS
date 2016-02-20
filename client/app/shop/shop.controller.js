fb313d2ab2d2f6dccc895a3b9abd9a9d6f3ce2af
Tom Claasens <thomas.claasens@outlook.com>
2016-02-20 10:44:52 +0200

    Update to homepage

--- client/app/shop/shop.controller.js
+++ client/app/shop/shop.controller.js
@@ -1,14 +1,17 @@
 'use strict';
 
 angular.module('jvapesApp')
-  .controller('ShopCtrl', function ($scope, $http, $log, socket) {
-     $http.get('/api/categories').then(response => {
-        $log.info(response.data);
-        this.categories = response.data;
-        socket.syncUpdates('category', this.categories);
-      });
+    .controller('ShopCtrl', function ($scope, $http, $log, socket) {
+        this.categories = [];
+     
+        //Categories
+        $http.get('/api/categories').then(response => {
+            $log.info(response.data);
+            this.categories = response.data;
+            socket.syncUpdates('category', this.categories);
+        });
 
-      $scope.$on('$destroy', function () {
-        socket.unsyncUpdates('category');
-      });
-  });
+        $scope.$on('$destroy', function () {
+            socket.unsyncUpdates('category');
+        });
+    });
