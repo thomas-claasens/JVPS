79170af068005e56c8bd9e112cfa2c9ae069558e
Tom Claasens <thomas.claasens@outlook.com>
2016-03-01 06:34:37 +0200

    Update to some state params

--- client/app/shop/product/product/product.controller.js
+++ client/app/shop/product/product/product.controller.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('jvapesApp')
-    .controller('shopproductCtrl', function (Auth, $scope,$log, $stateParams, $http, socket) {
+    .controller('shopproductCtrl', function (Auth, $state, $scope,$log, $stateParams, $http, socket) {
         this.isLoggedIn = Auth.isLoggedIn;
         this.isAdmin = Auth.isAdmin;
         this.getCurrentUser = Auth.getCurrentUser;
         this.products = [];
-        
+        $log.info($state.params);
+        this.before = $state.params.referrer;
          //Categories
         $http.get('/api/products/category/' + $stateParams.id).then(response => {
             //$log.info(response.data);
--- client/app/shop/shop.controller.js
+++ client/app/shop/shop.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('jvapesApp')
-    .controller('ShopCtrl', function ($scope, $http, $log, socket) {
+    .controller('ShopCtrl', function ($scope, $state, $http, $log, socket) {
         this.categories = [];
-     
+        $log.info($state.params);
         //Categories
         $http.get('/api/categories').then(response => {
             $log.info(response.data);
